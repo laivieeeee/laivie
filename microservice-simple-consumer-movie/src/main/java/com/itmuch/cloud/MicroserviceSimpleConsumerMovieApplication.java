f280411c54f10f87cdbddbde4962d89f7ff79041
laivieeeee <707944442@qq.com>
2019-11-22 20:37:14 +0800

    提交微服务

---
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -0,0 +1,22 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MicroserviceSimpleConsumerMovieApplication {
+
+    /*
+     * 使用restTemplate调用微服务api
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+    }
+}
